f16e54db9371b4884d61666267826d41e79c2712
Neiios <68248740+Neiios@users.noreply.github.com>
2020-08-23 16:07:20 +0000

    Update AM.sh

--- AM.sh
+++ AM.sh
@@ -2,9 +2,20 @@
 
 timedatectl set-ntp true
 
+pacman -Sy
+
+#Making partitions.
+sgdisk -n 1:0:+512M /dev/sda1
+sgdisk -n 2:0:0     /dev/sda2
+
+sgdisk -t 1:ef00 /dev/sda1
+sgdisk -t 2:8300 /dev/sda2
+
+#Formatting drive.
 mkfs.vfat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
 
+#Mounting created partitions.
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mkdir /mnt/boot/efi
