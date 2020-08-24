6eb3cf6efd2853b744bf2c4d75a45a92bac9b490
Neiios <68248740+Neiios@users.noreply.github.com>
2020-08-24 17:03:49 +0000

    Update AM2.sh

--- AM2.sh
+++ AM2.sh
@@ -1,7 +1,23 @@
 pacman -S networkmanager dhclient --noconfirm --needed
 systemctl enable --now NetworkManager
 
-echo "Enter root password"
 passwd root
 
-exit
+ln -sf /usr/share/zoneinfo/Europe/Vilnius /etc/localtime
+hwclock --systohc
+
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+
+echo Summit > /etc/hostname
+echo 127.0.1.1 localhost.localdomain Summit > /etc/hosts
+
+bootctl install
+cat <<EOF > /boot/loader/entries/arch.conf
+title Arch Linux  
+linux /vmlinuz-linux  
+initrd  /initramfs-linux.img  
+options root=/dev/sda1 rw
+EOF
+
+reboot
