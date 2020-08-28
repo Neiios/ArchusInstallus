274c22cc4935e3363945adf262a6f901beab89f7
Neiios <68248740+Neiios@users.noreply.github.com>
2020-08-28 18:47:54 +0300

    Update AM2.sh

--- AM2.sh
+++ AM2.sh
@@ -1,4 +1,4 @@
-pacman -S networkmanager dhclient --noconfirm --needed
+pacman -S networkmanager grub dhclient efibootmgr os-prober wpa-suplicant dialog mtools network-manager-applet ntfs-3g dosfstools linux-headers --noconfirm --needed
 systemctl enable --now NetworkManager
 
 passwd root
@@ -12,6 +12,15 @@ locale-gen
 echo Summit > /etc/hostname
 echo 127.0.1.1 localhost.localdomain Summit > /etc/hosts
 
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
 
+useradd -m -G wheel neiios
+
+sed -i 's/^# %wheel ALL=(ALL) ALL NOPASSWD: ALL/%wheel ALL=(ALL) ALL NOPASSWD: ALL/' /etc/sudoers.tmp
+
+pacman -S pulseaudio pulseaudio-alsa xorg xorg-xinit xorg-server xfce4 lightdm lightdm-gtk-greeter firefox
+echo "exec startxfce4" > ~/.xinitrc
+systemctl enable lightdm
 
 reboot
