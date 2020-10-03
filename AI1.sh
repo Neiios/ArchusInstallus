77d906e798a5665d58a8441e0b550a35b8421dca
Neiios <68248740+Neiios@users.noreply.github.com>
2020-10-03 14:53:26 +0300

    Update and rename AM1.sh to AI1.sh

--- AI1.sh
+++ AI1.sh
@@ -28,9 +28,9 @@ mkdir /mnt/boot
 mount -t vfat /dev/sda5 /mnt/boot
 
 pacstrap /mnt base base-devel linux linux-firmware vim --noconfirm --needed
-cp AM2.sh /mnt/AM2.sh
+cp AI2.sh /mnt/AI2.sh
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt /bin/bash ./AM2.sh
+arch-chroot /mnt /bin/bash ./AMI.sh
 
 umount -R /mnt
 reboot
